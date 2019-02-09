e0ffa33d9324a921869ceb053e273038e80f01b3
KvNGCzA <akanmuoluwadamilare@yahoo.com>
2019-02-09 21:35:41 +0100

    initial commit

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ const port = process.env.PORT;
 console.log(port);
 const app = express();
 
-app.get('*', (req, res) => res.json('server up'))
+app.get('*', (req, res) => res.json({ message: 'server up' }));
 
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}`));
 
 export default app;
